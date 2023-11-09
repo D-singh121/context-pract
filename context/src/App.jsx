cd04a73bf65e841d7c310bc28cd3a4003008fec0
Devesh_singh <choudharydevesh121@gmail.com>
2023-11-09 16:04:30 +0530

    added both pract

---
+++ context/src/App.jsx
@@ -0,0 +1,12 @@
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+function App() {
+
+  return (
+    <UserContextProvider>
+    <h1>Hello friends lets learn context Api</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ context/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // ------ useContext hook ki sahayata se hi hum Apne store UserContext ko access kar sakte hai ./
+  const { setUser } = useContext(UserContext);  //** setUser ki madad se hum context me data bhej sakte hai   */
+
+  const handleSubmit = (e) => {
+    e.PreventDefault(); //**** value url ke through kahi jaye na iskeliy preventDefault */
+    setUser({ username, password }); //****  hum deta bhej rahe hai through context */
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        type="text"
+        placeholder="Username"
+      />
+
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="text"
+        placeholder="Password"
+      />
+
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default Login;
