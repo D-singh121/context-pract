5fbaa930b5fa6d1e77bee4e762abe3514e1dd0b4
Devesh_singh <choudharydevesh121@gmail.com>
2023-11-10 15:43:48 +0530

    added TodoItems components and modified app.jsx

--- 04Todo-list/src/App.jsx
+++ 04Todo-list/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoContextProvider } from "./contexts";
+import TodoForm from "./components/TodoForm";
+import TodoItems from "./components/TodoItems";
 
 function App() {
   //***** hume todo context ko store karne ke liye state chahiye thats why we are using useState*/
@@ -28,7 +30,7 @@ function App() {
     //**** sabse pahle hum previous array ki state ka access lenge usme fir map lagayenge to uski sabhi values ka access mil jayega fir hum useme new toggle id ko match karenge agar id match hui to hum completed property ki boolean value ko revers kar denge ! se   */
     setTodos((prev) =>
       prev.map((prevTodo) =>
-        prevTodo === id
+        prevTodo.id === id
           ? { ...prevTodo, completed: !prevTodo.completed }
           : prevTodo
       )
@@ -59,9 +61,16 @@ function App() {
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">
               Manage Your Todos
             </h1>
-            <div className="mb-4">{/* Todo form goes here */}</div>
+            <div className="mb-4">
+              <TodoForm />
+            </div>
             <div className="flex flex-wrap gap-y-3">
               {/*Loop and Add TodoItem here */}
+              {todos.map((todo) => (
+                <div key={todo.id} className="w-full">
+                  <TodoItems todo={todo} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
